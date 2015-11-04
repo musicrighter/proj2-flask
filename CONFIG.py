912cb4a2f6fa5578be94f2728419afb8315877bc
David Gustafson <djg@ix-trusty.cs.uoregon.edu>
2015-11-03 18:25:42 -0800

    works on ix!

--- CONFIG.py
+++ CONFIG.py
@@ -6,11 +6,11 @@ Edit to fit development or deployment environment.
 import random 
 
 ### My local development environment
-SERVER_NAME = "localhost:5000"
-PORT=5000
-DEBUG = True
+#SERVER_NAME = "localhost:5000"
+#PORT=5000
+#DEBUG = True
 
 ### On ix.cs.uoregon.edu
-#SERVER_NAME = "ix.cs.uoregon.edu:{}".format(random.randint(5000,8000))
-#PORT="{}".format(random.randint(5000,8000))
-#DEBUG = False # Because it's unsafe to run outside localhost
+SERVER_NAME = "ix.cs.uoregon.edu:{}".format(random.randint(5000,8000))
+PORT="{}".format(random.randint(5000,8000))
+DEBUG = False # Because it's unsafe to run outside localhost
--- app.cgi
+++ app.cgi
@@ -3,7 +3,7 @@
 """ For deployment on ix under CGI """
 
 import site
-site.addsitedir("/home/faculty/michal/public_html/htbin/cis322/proj2-flask/env/lib/python3.4/site-packages")
+site.addsitedir("/home/users/djg/public_html/proj2-flask/env/lib/python3.4/site-packages")
 
 from wsgiref.handlers import CGIHandler
 from syllabus import app
